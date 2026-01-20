bacde008fe4b49d5fd4a5180062bddbe403a5a47
Viktoria <airotkiv15@gmail.com>
2026-01-19 21:50:13 -0800

    firebase

--- firebase.js
+++ firebase.js
@@ -3,9 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyBaYm2yrrxROKVpiOGnHDkegKu9H_28Jys",
+  authDomain: "moonthly-6bd9b.firebaseapp.com",
+  projectId: "moonthly-6bd9b",
+  storageBucket: "moonthly-6bd9b.firebasestorage.app",
+  messagingSenderId: "784910931143",
+  appId: "1:784910931143:web:f68e5d6bb88545bd901ae2"
 };
 
 export const app = initializeApp(firebaseConfig);
